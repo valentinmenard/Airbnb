248c628c1e3618a3d1055704ca5e72554dff8c4f
nicolas biencourt <nicolasbiencourt@me.com>
2015-04-24 10:38:42 +0200

    updater l'image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    p params
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :picture) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :avatar) }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423134748) do
+ActiveRecord::Schema.define(version: 20150424082700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,10 +80,14 @@ ActiveRecord::Schema.define(version: 20150423134748) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
-    t.string   "picture"
     t.string   "name"
+    t.string   "picture"
     t.string   "token"
     t.datetime "token_expiry"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
