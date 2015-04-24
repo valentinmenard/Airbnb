4f01113a592c896b14eeb10262b6621748865c90
nicolas biencourt <nicolasbiencourt@me.com>
2015-04-24 12:13:02 +0200

    accepted bookings and home navabar modifs

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,11 +17,19 @@ class BookingsController < ApplicationController
     end
   end
 
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update_attributes(update_booking_params)
+    redirect_to :back
+  end
 
   private
+
+  def update_booking_params
+    params.require(:booking).permit(:accepted)
+  end
 
   def booking_params
     params.require(:booking).permit(:starting_on, :ending_on, :travellers)
   end
-
 end
