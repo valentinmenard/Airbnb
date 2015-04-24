895506e368c06ae36dd0831d26009ce2e357c5b8
Valentin Menard <valentin.menard@essec.edu>
2015-04-24 12:42:31 +0200

    searchbar

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,11 +3,17 @@ class FlatsController < ApplicationController
   skip_before_filter :authenticate_user!, only: [:index, :show]
 
   def index
-    @flats = Flat.all
+    @flats = Flat.where(city: search_params[:city])
   end
 
   def show
     @flat = Flat.find(params[:id])
+  end
+
+  private
+
+  def search_params
+    params.permit(:city, :travellers, :starting_on, :ending_on )
   end
 
 end
